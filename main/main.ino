283435a664dea317b49e46f513f635facafc9c8e
cosmin <cosminmoarcas01@gmail.com>
2024-01-20 22:05:49 +0200

    some refactoring

--- main/main.ino
+++ main/main.ino
@@ -1,13 +1,15 @@
 #include <QTRSensors.h>
 
-const int m11Pin = 7;
-const int m12Pin = 6;
-const int m21Pin = 5;
-const int m22Pin = 4;
-const int m1Enable = 11;
-const int m2Enable = 10;
-int m1Speed = 0;
-int m2Speed = 0;
+
+const int motorLeftForwardPin = 7;   // Pinul pentru mers înainte la motorul stâng
+const int motorLeftReversePin = 6;   // Pinul pentru mers înapoi la motorul stâng
+const int motorRightForwardPin = 5;  // Pinul pentru mers înainte la motorul drept
+const int motorRightReversePin = 4;  // Pinul pentru mers înapoi la motorul drept
+const int motorLeftSpeedPin = 11;   // Pinul pentru controlul vitezei motorului stang
+const int motorRightSpeedPin = 10;  // Pinul pentru controlul vitezei motorului drept
+
+int motorLeftSpeed = 0;
+int motorRightSpeed = 0;
 // increase kp’s value and see what happens
 float kp = 1;
 float ki = 0;
@@ -27,49 +29,49 @@ int sensors[sensorCount] = { 0, 0, 0, 0, 0, 0 };
 
 void setup() {
   // pinMode setup
-  pinMode(m11Pin, OUTPUT);
-  pinMode(m12Pin, OUTPUT);
-  pinMode(m21Pin, OUTPUT);
-  pinMode(m22Pin, OUTPUT);
-  pinMode(m1Enable, OUTPUT);
-  pinMode(m2Enable, OUTPUT);
+  pinMode(motorLeftForwardPin, OUTPUT);
+  pinMode(motorLeftReversePin, OUTPUT);
+  pinMode(motorRightForwardPin, OUTPUT);
+  pinMode(motorRightReversePin, OUTPUT);
+  pinMode(motorLeftSpeedPin, OUTPUT);
+  pinMode(motorRightSpeedPin, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
   qtr.setTypeAnalog();
   qtr.setSensorPins((const uint8_t[]){ A0, A1, A2, A3, A4, A5 }, sensorCount);
   delay(500);
-  pinMode(LED_BUILTIN, OUTPUT);
+  
+  // calibrate sensor
   digitalWrite(LED_BUILTIN, HIGH);  // turn on Arduino's LED to indicate we are in calibration mode
-  // calibrate the sensor. For maximum grade the line follower should do the movement itself, without human interaction.
-  for (uint16_t i = 0; i < 400; i++) {
-    qtr.calibrate();
-    // do motor movement here, with millis() as to not ruin calibration)
-  }
+  calibrateSensor();
   digitalWrite(LED_BUILTIN, LOW);
+
   Serial.begin(9600);
 }
+
 void loop() {
   // inefficient code, written in loop. You must create separate functions
   int error = map(qtr.readLineBlack(sensorValues), 0, 5000, -50, 50);
   p = error;
   i = i + error;
   d = error - lastError;
-  int motorSpeed = kp * p + ki * i + kd * d;  // = error in this case
-  m1Speed = baseSpeed;
-  m2Speed = baseSpeed;
+  int correctionSpeed = kp * p + ki * i + kd * d;
+  int motorLeftSpeed = baseSpeed;
+  int motorRightSpeed = baseSpeed;
   // a bit counter intuitive because of the signs
   // basically in the first if, you substract the error from m1Speed (you add the negative)
   // in the 2nd if you add the error to m2Speed (you substract the negative)
   // it's just the way the values of the sensors and/or motors lined up
   if (error < 0) {
-    m1Speed += motorSpeed;
+    motorLeftSpeed += correctionSpeed;
   } else if (error > 0) {
-    m2Speed -= motorSpeed;
-  }
+    motorRightSpeed -= correctionSpeed;
+  } 
   // make sure it doesn't go past limits. You can use -255 instead of 0 if calibrated programmedproperly.
   // making sure we don't go out of bounds
   // maybe the lower bound should be negative, instead of 0? This of what happens when making asteep turn
-  m1Speed = constrain(m1Speed, 0, maxSpeed);
-  m2Speed = constrain(m2Speed, 0, maxSpeed);
-  setMotorSpeed(m1Speed, m2Speed);
+  motorLeftSpeed = constrain(motorLeftSpeed, 0, maxSpeed);
+  motorRightSpeed = constrain(motorRightSpeed, 0, maxSpeed);
+  setMotorSpeed(motorLeftSpeed, motorRightSpeed);
   // DEBUGGING
   // Serial.print("Error: ");
   // Serial.println(error);
@@ -86,41 +88,43 @@ void loop() {
 void pidControl(float kp, float ki, float kd) {
   // TODO
 }
+
 // each arguments takes values between -255 and 255. The negative values represent the motor speedin reverse.
-void setMotorSpeed(int motor1Speed, int motor2Speed) {
-  // remove comment if any of the motors are going in reverse
-  // motor1Speed = -motor1Speed;
-  // motor2Speed = -motor2Speed;
-  if (motor1Speed == 0) {
-    digitalWrite(m11Pin, LOW);
-    digitalWrite(m12Pin, LOW);
-    analogWrite(m1Enable, motor1Speed);
+void setMotorSpeed(int motorLeftSpeed, int motorRightSpeed) {
+  if (motorLeftSpeed == 0) {
+    digitalWrite(motorLeftForwardPin, LOW);
+    digitalWrite(motorLeftReversePin, LOW);
   } else {
-    if (motor1Speed > 0) {
-      digitalWrite(m11Pin, HIGH);
-      digitalWrite(m12Pin, LOW);
-      analogWrite(m1Enable, motor1Speed);
+    if (motorLeftSpeed > 0) {
+      digitalWrite(motorLeftForwardPin, HIGH);
+      digitalWrite(motorLeftReversePin, LOW);
     }
-    if (motor1Speed < 0) {
-      digitalWrite(m11Pin, LOW);
-      digitalWrite(m12Pin, HIGH);
-      analogWrite(m1Enable, -motor1Speed);
+    if (motorLeftSpeed < 0) {
+      digitalWrite(motorLeftForwardPin, LOW);
+      digitalWrite(motorLeftReversePin, HIGH);
+      motorLeftSpeed = -motorLeftSpeed;
     }
   }
-  if (motor2Speed == 0) {
-    digitalWrite(m21Pin, LOW);
-    digitalWrite(m22Pin, LOW);
-    analogWrite(m2Enable, motor2Speed);
+   if (motorRightSpeed == 0) {
+    digitalWrite(motorRightForwardPin, LOW);
+    digitalWrite(motorRightReversePin, LOW); 
   } else {
-    if (motor2Speed > 0) {
-      digitalWrite(m21Pin, HIGH);
-      digitalWrite(m22Pin, LOW);
-      analogWrite(m2Enable, motor2Speed);
+    if (motorRightSpeed > 0) { 
+      digitalWrite(motorRightForwardPin, HIGH);
+      digitalWrite(motorRightReversePin, LOW);
     }
-    if (motor2Speed < 0) {
-      digitalWrite(m21Pin, LOW);
-      digitalWrite(m22Pin, HIGH);
-      analogWrite(m2Enable, -motor2Speed);
+    if (motorRightSpeed < 0) {
+      digitalWrite(motorRightForwardPin, LOW);
+      digitalWrite(motorRightReversePin, HIGH);
+      motorRightSpeed = -motorRightSpeed;
     }
+  }
+  analogWrite(motorLeftSpeedPin, motorLeftSpeed);
+  analogWrite(motorRightSpeedPin, motorRightSpeed);
+}
+
+void calibrateSensor() {
+  while (millis() < 10000) {
+    qtr.calibrate();
   }
 }
